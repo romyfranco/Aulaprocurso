e5332cf3bd5207296bb6a47f0b7217dfd8ce7f08
romyfranco <igmarfranco@hotmail.com>
2026-07-23 07:03:06 -0500

    Reconoce estilos Reveal integrados

--- tests/Feature/RevealPresentationTest.php
+++ tests/Feature/RevealPresentationTest.php
@@ -251,6 +251,7 @@ class RevealPresentationTest extends TestCase
         $asset->assertSee('voranapro:reveal-prepared', false);
         $asset->assertSee('voranapro:reveal-visible', false);
         $asset->assertSee('revealStylesAreApplied', false);
+        $asset->assertSee("document.querySelector('[data-voranapro-inlined-stylesheet]')", false);
         $asset->assertDontSee('root.clientWidth < 1 || root.clientHeight < 1', false);
         $asset->assertSee('ensureRevealRuntime', false);
         $asset->assertSee('voranapro_script_retry', false);
